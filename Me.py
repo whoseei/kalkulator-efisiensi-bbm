69ad3ce146047c5f32a9eab6f824cc58951fdcba
whoseei <ifhamusdalifa82@gmail.com>
2026-03-14 22:16:17 +0800

    add all files

---
+++ Me.py
@@ -0,0 +1,30 @@
+import requests
+
+response = requests.get("https://siakad.unm.ac.id/")
+print(response.status_code)
+print(response.text)
+
+import math
+
+print(math.sqrt(67))
+print(math.pi)
+
+import random
+
+angka = random.randint(6, 70)
+print("Angka acak:", angka)
+
+from datetime import datetime
+
+sekarang = datetime.now()
+print("Waktu sekarang:", sekarang)
+
+import random
+
+nilai = random.randint(2, 67)
+print("Nilai:", nilai)
+
+if nilai >= 44:
+    print("Lulus")
+else:
+    print("Tidak Lulus")
